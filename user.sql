b817f0fc1b4d4b66b5d537cc87d35a4a89986acc
wenlan123 <84956741+wenlan123@users.noreply.github.com>
2023-05-11 17:15:32 +0800

    Add files via upload
    这是我自己写的一个前后台的纯html的  大家可以一起完善  有什么需求也可以告诉我

---
+++ user.sql
@@ -0,0 +1,44 @@
+/*
+SQLyog  v12.2.6 (64 bit)
+MySQL - 8.0.32 : Database - wenshududu
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`wenshududu` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `wenshududu`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(20) COLLATE utf8mb4_bin NOT NULL COMMENT '姓名',
+  `account` varchar(20) COLLATE utf8mb4_bin NOT NULL COMMENT '账号',
+  `password` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '密码',
+  `age` int DEFAULT '0' COMMENT '性别（0未知，1男，2女',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_pk2` (`account`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`name`,`account`,`password`,`age`) values 
+(1,'0','0','0',0),
+(2,'00001','00001','00001',0),
+(3,'12345','12345','12345',0),
+(4,'2','2','2',0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
